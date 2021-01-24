c6d98b323252d52720e393a624969535fa3d4732
MinimanC <casey.temple@knights.ucf.edu>
2021-01-24 12:02:44 -0500

    First commit

---
+++ Assets/Scripts/MissileController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileController : MonoBehaviour
+{
+    public Transform position;
+    public float xCenter = 0f;
+    private SpriteRenderer Sprite;
+
+    void Start()
+    {
+        position = this.transform.Find("Transform");
+        Sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(xCenter + Mathf.PingPong(Time.time * 2, 6) - 6/2f, transform.position.y, transform.position.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    public float speed;
+
+    public float time = 12;
+    public bool timeRun = false;
+
+    public Text startText;
+    public Text winText;
+    public Text loseText;
+    public Text timeText;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        speed = 0;
+        timeRun = true;
+        startText.text = "Use W and S to move. Avoid the Missiles.";
+        timeText.text = time.ToString("Time: " + time);
+        winText.text = "";
+        loseText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = 0;
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2d.AddForce (movement * speed);
+    }
+
+    void Update()
+    {
+        if (timeRun)
+        {
+            if (time <= 10)
+            {
+                speed = 20;
+                startText.text = "";
+            }
+
+            if (time > 0)
+            {
+                time -= Time.deltaTime;
+                timeText.text = time.ToString("Time: " + time);
+            }
+            else
+            {
+                time = 0;
+                timeRun = false;
+                speed = 0;
+                loseText.text = "Game Over.";
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Win")
+        {
+            speed = 0;
+            winText.text = "You win! Game created by Casey Temple.";
+            timeRun = false;
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            speed = 0;
+            loseText.text = "Game Over.";
+            timeRun = false;
+        }
+    }
+}
